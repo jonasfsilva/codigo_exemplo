76d46c284f6adc659bbf06d420b99a929f153de9
jonasfsilva <ferreirajonasss@gmail.com>
2020-02-12 09:50:23 -0300

    add changes

--- rispartest.py
+++ rispartest.py
@@ -9,41 +9,15 @@ parser.add_argument('contas_csv')
 parser.add_argument('transacoes_csv')
 args = parser.parse_args()
 
-def read_contas_csv(filename):
-    with open(filename) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
-            print(Conta(row[0], row[1]))
-
-def read_transacoes_csv(filename):
-    with open(filename) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
-            print(Transacao(row[0], row[1]))
 
 def main():
-    contas_csv = args.contas_csv
-    transacoes_csv = args.transacoes_csv
 
-    contas = ManageOperations.read_csv(contas_csv)
-    transacoes = ManageOperations.read_csv(transacoes_csv)
+    manage_operations = ManageOperations()
+    manage_operations.load_data(args.contas_csv, args.transacoes_csv)
+    manage_operations.execute()
     
-    ManageOperations.load_contas()
-    ManageOperations.load_transacoes()
-
-    # opcoes passar os dois csvs pra dentro da classe manager
-    # ou usar como interface fazendo operacoes no main
-
-    manager_instance = ManageOperations(contas_csv, transacoes_csv)
-    manager_instance.load_contas
-    manager_instance.load_transacoes
-    manager_instance.apply_transacoes
+    #manager_instance.show_saldos()
     
-    manager_instance.show_saldos()
-    
-    # read_contas_csv(contas_csv)
-    # read_transacoes_csv(transacoes_csv)
-
     return 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,37 +1,33 @@
 import csv
-from collections import OrderedDict 
+from collections import OrderedDict
+from transacoes import Transacao 
+from contas import Conta 
 
 
 class ManageOperations:
 
-    contas_csv = ''
-    transacoes_csv = ''
     contas = []
     transacoes = []
-
-    def __init__(self, contas_csv, transacoes_csv):
-        self.contas_csv = contas_csv
-        self.transacoes_csv = transacoes_csv
 
     def read_csv(self, filename):
         with open(filename) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
+            lines = OrderedDict() 
             for row in csv_reader:
-                lines = OrderedDict() 
                 lines[row[0]] = row[1]
         return lines
 
-    def load_data(self):
-        transacoes_lines = self.read_csv(self.transacoes_csv)
-        contas_lines = self.read_csv(self.contas_csv)
+    def load_data(self, contas_csv, transacoes_csv):
+        transacoes_lines = self.read_csv(transacoes_csv)
+        contas_lines = self.read_csv(contas_csv)
 
-        for conta in contas_lines:
-            pass
+        for conta_id, conta_saldo  in contas_lines.items():
+            self.contas.append(Conta(conta_id, conta_saldo))
 
-        for transacao in transacoes_lines:
-            pass
+        for transacao_id, transacao_valor in transacoes_lines.items():
+            self.transacoes.append(Transacao(transacao_id, transacao_valor))
         
-    def run(self):
+    def execute(self):
         #contas que tem saldos
         #percorrer as transacoes pegar as contas e aplicar as transacoes
 
